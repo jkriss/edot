8e04ddf0697c447997d0459b98fdf08bc37ec9c5
Jesse Kriss <jesse@jklabs.net>
2010-03-20 15:32:41 -0700

    adding feed to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
   def index
     @beta_signup = BetaSignup.new
     @count = BetaSignup.count
+    
+    @latest_things = Thing.find(:all, :order => "created_at desc", :limit => 10)
   end
   
 end
