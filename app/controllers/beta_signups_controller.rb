464eb8d103337422440f6a599e77100692749f0e
Jesse Kriss <jesse@jklabs.net>
2010-03-20 07:47:30 -0700

    adding beta signup controller

---
+++ app/controllers/beta_signups_controller.rb
@@ -0,0 +1,13 @@
+class BetaSignupsController < ApplicationController
+  
+  def create
+    @beta_signup = BetaSignup.new(params[:beta_signup])
+    if @beta_signup.save
+      flash[:message] = "Thanks for signing up!"
+    else
+      flash[:message] = "You're already signed up! Thanks for your enthusiasm"
+    end
+    redirect_to root_path
+  end
+  
+end
--- app/models/beta_signup.rb
+++ app/models/beta_signup.rb
@@ -1,2 +1,5 @@
 class BetaSignup < ActiveRecord::Base
+  
+  validates_uniqueness_of :email_address
+  
 end
