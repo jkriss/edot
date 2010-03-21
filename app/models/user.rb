29466a9f348080b67a635ccbeedd0fd172430219
Jesse Kriss <jesse@jklabs.net>
2010-03-21 11:19:47 -0700

    allowing multiple nil emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < TwitterAuth::GenericUser
 
   # validates_presence_of     :email
   # validates_length_of       :email,    :within => 6..100 #r@a.wk
-  validates_uniqueness_of   :email
+  validates_uniqueness_of   :email, :allow_nil => true
   # validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
   before_save :update_cached_points
