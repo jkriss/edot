7dca8d598e679af24d3579273394981fcd563ae7
Jesse Kriss <jesse@jklabs.net>
2010-03-20 17:16:58 -0700

    adding helper to make time pacific time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def local(date)
+    TZInfo::Timezone.get('America/Los_Angeles').utc_to_local(date)
+  end
+  
 end
