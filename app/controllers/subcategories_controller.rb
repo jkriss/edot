4e6d1b4732b43276b19364565a2282aaccd026aa
Jesse Kriss <jesse@jklabs.net>
2010-03-24 10:26:17 -0700

    creating new subcategories should work now. pulling delete links, too.

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -16,6 +16,11 @@ class SubcategoriesController < ApplicationController
     @subcategory.update_attributes!(params[:subcategory])
     redirect_to edit_category_path(@subcategory.category)
   end
-
   
+  def create
+    @category = Category.find(params[:category_id])
+    @subcategory = @category.subcategories.create!(params[:subcategory])
+    redirect_to edit_category_path(@subcategory.category)
+  end
+
 end
