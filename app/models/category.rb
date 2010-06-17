dfdd4f60b73df30ed00fc4c5699727f44ee7a257
Jesse Kriss <jesse@jklabs.net>
2010-06-17 17:13:58 -0400

    adding much cleaner reset for monthly category leaders

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,7 @@ class Category < ActiveRecord::Base
   has_many :subcategories, :through => :category_subcategories
   belongs_to :top_user, :class_name => 'User'
   
+  
   def total_points(conditions={})
     total = 0
     subcategories.each do |s|
@@ -19,7 +20,14 @@ class Category < ActiveRecord::Base
     total_points(['user_id = ? and created_at > ?', user.id, start_date] )
   end
   
+  def top_user_with_check
+    find_top_user if top_user_updated_at < Time.now.beginning_of_month
+    top_user_without_check
+  end
+  alias_method_chain :top_user, :check
+  
   def find_top_user
+    logger.warn "calculating top user for #{self.name}"
     self.top_user_score = 0
     self.top_user = nil
     User.all.each do |u|
